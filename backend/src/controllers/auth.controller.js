d667e44b5b2ea7754dd3c0df1b0478d5569c52ec
JQSPx <joao.quintilianosouza@gmail.com>
2026-07-26 20:18:44 -0300

    refactor: move logica de autenticacao para o service

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,56 +1,25 @@
-import bcrypt from "bcrypt";
-import prisma from "../services/prisma.js";
-import jwt from "jsonwebtoken";
+import AuthService from "../services/auth.service.js";
 
 const AuthController = {
+  //registrar user
   async register(req, res, next) {
     try {
-      const { nome, email, senha, role } = req.body;
-      const userExists = await prisma.user.findUnique({ where: { email } });
-      if (userExists) {
-        return res.status(400).json({ error: "Este e-mail já está em uso." });
-      }
+      const { nome, email, senha } = req.body;
 
-      const hashedPassword = await bcrypt.hash(senha, 10);
+      const newUser = await AuthService.registrar({ nome, email, senha });
 
-      const newUser = await prisma.user.create({
-        data: {
-          nome,
-          email,
-          senha: hashedPassword,
-          role: role || "TECNICO",
-        },
-      });
-
-      delete newUser.senha;
       return res.status(201).json(newUser);
     } catch (error) {
       next(error);
     }
   },
-
+  //logar
   async login(req, res, next) {
     try {
       const { email, senha } = req.body;
-      const user = await prisma.user.findUnique({ where: { email } });
-      if (!user || !user.ativo) {
-        return res
-          .status(401)
-          .json({ error: "Credenciais inválidas ou usuário inativo" });
-      }
 
-      const senhaValida = await bcrypt.compare(senha, user.senha);
-      if (!senhaValida) {
-        return res.status(401).json({ error: "Credenciais inválidas" });
-      }
+      const { user, token } = await AuthService.login({ email, senha });
 
-      const token = jwt.sign(
-        { id: user.id, role: user.role },
-        process.env.JWT_SECRET || "chave_secreta_fallback",
-        { expiresIn: "1d" },
-      );
-
-      delete user.senha;
       return res.json({ user, token });
     } catch (error) {
       next(error);
