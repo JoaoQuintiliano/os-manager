93ad2b25b742d5c794b460fae00da203f68c4e73
JQSPx <joao.quintilianosouza@gmail.com>
2026-07-27 09:37:42 -0300

    refactor: move a logica de ordemServico.controller para ordemServico.service

--- backend/src/controllers/ordemServico.controller.js
+++ backend/src/controllers/ordemServico.controller.js
@@ -1,34 +1,20 @@
-import prisma from "../services/prisma.js";
+import OrdemServicoService from "../services/ordemServico.service.js";
 
 const OrdemServicoController = {
+  //armazenar os
   async store(req, res, next) {
     try {
       const usuarioId = req.user.id;
       const { descricao, prioridade, prazoSLA, clienteId, tecnicoId } =
         req.body;
 
-      const resultado = await prisma.$transaction(async (tx) => {
-        const os = await tx.ordemServico.create({
-          data: {
-            descricao,
-            prioridade,
-            prazoSLA: new Date(prazoSLA),
-            clienteId: Number(clienteId),
-            tecnicoId: tecnicoId ? Number(tecnicoId) : null,
-            status: "ABERTA",
-          },
-        });
-
-        await tx.historicoOS.create({
-          data: {
-            os: { connect: { id: os.id } },
-            usuario: { connect: { id: Number(usuarioId) } },
-            acao: "ABERTURA",
-            observacao: "Ordem de serviço aberta no sistema.",
-          },
-        });
-
-        return os;
+      const resultado = await OrdemServicoService.criar({
+        descricao,
+        prioridade,
+        prazoSLA,
+        clienteId,
+        tecnicoId,
+        usuarioId,
       });
 
       return res.status(201).json(resultado);
@@ -36,39 +22,24 @@ const OrdemServicoController = {
       next(error);
     }
   },
-
+  //editar status os
   async updateStatus(req, res, next) {
     try {
       const { id } = req.params;
       const { novoStatus, observacao } = req.body;
-
       const usuarioId = req.user?.id;
 
       if (!usuarioId) {
-        return res.status(401).json({
-          error: "Usuário não autenticado. Faça login primeiro.",
-        });
+        return res
+          .status(401)
+          .json({ error: "Usuário não autenticado. Faça login primeiro." });
       }
 
-      const osAtualizada = await prisma.$transaction(async (tx) => {
-        const os = await tx.ordemServico.update({
-          where: { id: Number(id) },
-          data: {
-            status: novoStatus,
-            closedAt: novoStatus === "FINALIZADA" ? new Date() : null,
-          },
-        });
-
-        await tx.historicoOS.create({
-          data: {
-            acao: `ALTERAÇÃO DE STATUS: ${novoStatus}`,
-            observacao: observacao || `Status alterado para ${novoStatus}`,
-            os: { connect: { id: os.id } },
-            usuario: { connect: { id: usuarioId } },
-          },
-        });
-
-        return os;
+      const osAtualizada = await OrdemServicoService.atualizarStatus({
+        id,
+        novoStatus,
+        observacao,
+        usuarioId,
       });
 
       return res.json(osAtualizada);
@@ -76,8 +47,7 @@ const OrdemServicoController = {
       next(error);
     }
   },
-
-  // EXIBIR OS, SHORT + FILTRO
+  //exibir os e filtro dashboard
   async index(req, res, next) {
     try {
       const {
@@ -87,116 +57,53 @@ const OrdemServicoController = {
         ordenar = "createdAt",
         direcao = "desc",
       } = req.query;
-      const camposPermitidos = ["id", "status", "prioridade", "createdAt"];
-      const camposOrdenacao = camposPermitidos.includes(ordenar)
-        ? ordenar
-        : "createdAt";
 
-      const direcaoOrdenacao = direcao === "asc" ? "asc" : "desc";
-      const filtroWhere = {};
-      if (status) {
-        filtroWhere.status = status;
-      }
-      if (prioridade) {
-        filtroWhere.prioridade = prioridade;
-      }
-
-      if (busca) {
-        filtroWhere.cliente = {
-          is: {
-            nome: {
-              contains: busca,
-            },
-          },
-        };
-      }
-      const ordens = await prisma.ordemServico.findMany({
-        where: filtroWhere,
-        include: {
-          cliente: { select: { nome: true, telefone: true } },
-          tecnico: { select: { nome: true } },
-        },
-        orderBy: { [camposOrdenacao]: direcaoOrdenacao },
+      const ordens = await OrdemServicoService.listar({
+        busca,
+        status,
+        prioridade,
+        ordenar,
+        direcao,
       });
+
       return res.json(ordens);
     } catch (error) {
       next(error);
     }
   },
+  //buscar por id
   async show(req, res, next) {
     try {
       const { id } = req.params;
-      const os = await prisma.ordemServico.findUnique({
-        where: { id: Number(id) },
-        include: {
-          cliente: true,
-          //  tecnico: true,
-          historicos: {
-            orderBy: { createdAt: "desc" },
-            include: { usuario: true },
-          },
-        },
-      });
+      const os = await OrdemServicoService.exibir({ id });
 
-      if (!os) return res.status(404).json({ error: "OS não encontrada" });
+      if (!os) {
+        return res.status(404).json({ error: "OS não encontrada" });
+      }
+
       return res.json(os);
     } catch (error) {
-      console.error(error);
       next(error);
     }
   },
-
-  async atribuirTecnico(req, res) {
+  //atribuir um tecnic
+  async atribuirTecnico(req, res, next) {
     try {
       const { id } = req.params;
       const { tecnicoId } = req.body;
       const usuarioLogadoId = req.user.id;
 
-      const os = await prisma.ordemServico.findUnique({
-        where: { id: Number(id) },
-      });
-
-      if (!os) {
-        return res
-          .status(404)
-          .json({ error: "Ordem de Serviço não encontrada." });
-      }
-
-      const tecnico = await prisma.user.findUnique({
-        where: { id: Number(tecnicoId) },
-      });
-
-      if (!tecnico || tecnico.role !== "TECNICO") {
-        return res
-          .status(400)
-          .json({ error: "Técnico inválido ou não encontrado." });
-      }
-
-      const resultado = await prisma.$transaction(async (tx) => {
-        const osAtualizada = await tx.ordemServico.update({
-          where: { id: Number(id) },
-          data: { tecnicoId: Number(tecnicoId) },
-        });
-
-        await tx.historicoOS.create({
-          data: {
-            os: { connect: { id: Number(id) } },
-            usuario: { connect: { id: Number(usuarioLogadoId) } },
-            acao: "TÉCNICO ATRIBUÍDO",
-            observacao: `OS atribuída ao técnico: ${tecnico.nome}`,
-          },
-        });
-
-        return osAtualizada;
+      const resultado = await OrdemServicoService.atribuirTecnico({
+        id,
+        tecnicoId,
+        usuarioLogadoId,
       });
 
       return res.json(resultado);
     } catch (error) {
-      console.error("Erro ao atribuir técnico:", error);
-      return res
-        .status(500)
-        .json({ error: "Erro interno do servidor ao atribuir técnico." });
+      next(error);
     }
   },
 };
+
 export default OrdemServicoController;
